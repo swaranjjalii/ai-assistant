59cc9840ef43563a939fefbbfc2da205e160438a
swaranjjalii <150799654+swaranjjalii@users.noreply.github.com>
2025-03-07 20:54:11 +0530

    Add files via upload

---
+++ test_data_processing.py
@@ -0,0 +1,10 @@
+import unittest
+from src.data_processing import preprocess_data
+
+class TestDataProcessing(unittest.TestCase):
+    def test_preprocess_data(self):
+        # Implement test for preprocess_data function
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_nlp_tasks.py
@@ -0,0 +1,10 @@
+import unittest
+from src.nlp_tasks import analyze_text
+
+class TestNLPTasks(unittest.TestCase):
+    def test_analyze_text(self):
+        # Implement test for analyze_text function
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
